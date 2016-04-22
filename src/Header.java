74692dd3e6df22512e13317d30013bcf487047d0
Barret J. Nobel <bear.nobel@gmail.com>
2016-04-22 13:26:28 -0400

    added comments

--- src/Header.java
+++ src/Header.java
@@ -7,8 +7,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Header {
-    private static final Map<String, Integer> NUM_OF_BITS = createMap();
-    private static final String VERSION        = "Version";
+    private static final Map<String, Integer> NUM_OF_BITS = createMap(); //..... variable to hold the Packet strings and array lengths
+    private static final String VERSION        = "Version"; //.................. final variables to be used for the hashmap keys
     private static final String HEADER_LENGTH  = "Header Length";
     private static final String SERVICE_TYPE   = "Service Type";
     private static final String TOTAL_LENGTH   = "Total Length";
@@ -20,11 +20,13 @@ public class Header {
     private static final String CHECKSUM       = "Checksum";
     private static final String SOURCE_ADDRESS = "Source Address";
     private static final String DEST_ADDRESS   = "Destination Address";
-    private static final int SIZE = 160;
+    private static final int SIZE = 160; //..................................... variable used to check if header size is legit
 
+    // Method to return a Map object representing
+    // the packet labels and size's for the array's
     private static Map<String, Integer> createMap() {
-        Map<String, Integer> result = new HashMap<String, Integer>();
-        result.put( VERSION, 4 );
+        Map<String, Integer> result = new HashMap<String, Integer>(); //. hashmap to return
+        result.put( VERSION, 4 ); //..................................... put all final key's written above with their values
         result.put( HEADER_LENGTH, 4);
         result.put( SERVICE_TYPE, 8);
         result.put( TOTAL_LENGTH, 16);
@@ -36,9 +38,10 @@ public class Header {
         result.put( CHECKSUM, 16);
         result.put( SOURCE_ADDRESS, 32);
         result.put( DEST_ADDRESS, 32);
-        return Collections.unmodifiableMap(result);
+        return Collections.unmodifiableMap(result); //.................. make it final
     }
 
+    // Method to test out the creation of hte hashmap
     public void printMap(){
         for( String key : NUM_OF_BITS.keySet() ){
             System.out.println( NUM_OF_BITS.get(key) );
